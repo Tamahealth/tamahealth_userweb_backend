b510207ec40fd497e3f3001f7bae3e52590363ea
Nathe Seas <natheseasn@Nathes-MacBook-Air.local>
2023-11-23 16:31:35 -0300

    progressed on the payment endpoints

--- routes/paymentRoutes.js
+++ routes/paymentRoutes.js
@@ -2,29 +2,46 @@ const express = require("express");
 const router = express.Router();
 const Stripe = require("stripe");
 const stripe = Stripe(process.env.STRIPE_SECRET_KEY);
-
 const { v4: uuidv4 } = require("uuid");
+const authenticateToken = require("../jwtMiddleware");
+const PaymentModel = require("../models/paymentModel");
+
+// get the service ID from services table
+router.get("/services/:serviceId", async (req, res) => {
+  try {
+    const serviceId = req.params.serviceId;
+    const serviceDetails = await PaymentModel.getServiceDetails(serviceId);
+    res.json(serviceDetails);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
 
 // POST endpoint to create a payment intent
-router.post("/create-payment-intent", async (req, res) => {
+router.post("/create-payment-intent", authenticateToken, async (req, res) => {
   try {
-    const { amount, serviceId, user_id } = req.body;
-    const user = req.user; // Assuming user info is set by authMiddleware
-
+    const { amount, serviceId, userId } = req.body;
+    console.log("Request body is ", req.body);
+    const user_id = req.user.userId; // Extracted from JWT token
+    console.log(
+      "userId from token is ",
+      userId,
+      "and user id from body is",
+      user_id
+    );
+    if (userId !== user_id) {
+      return res.status(403).json({ error: "Unauthorized action" });
+    }
     // Validate serviceId, amount, and user's eligibility for payment
-    const idempotencyKey = uuidv4(); // for avoiding duplicate charges
+    const idempotencyKey = uuidv4();
     const paymentIntent = await stripe.paymentIntents.create(
       {
         amount: amount,
         currency: "usd",
-        automatic_payment_methods: {
-          enabled: true,
-        },
-        metadata: { serviceId, userId: user_id },
+        automatic_payment_methods: { enabled: true },
+        metadata: { serviceId, userId: userId },
       },
-      {
-        idempotencyKey: idempotencyKey, // Ensure idempotency
-      }
+      { idempotencyKey }
     );
 
     res.json({ clientSecret: paymentIntent.client_secret });
